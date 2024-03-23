afb8ce8ef14db0ec13c2930871c471f44787ea40
ZhuRuoLing <3489382552@qq.com>
2024-03-23 14:27:35 +0800

    socket -> websocket

--- src/main/java/icu/takeneko/interact/network/NekoService.kt
+++ src/main/java/icu/takeneko/interact/network/NekoService.kt
@@ -2,17 +2,16 @@ package icu.takeneko.interact.network
 
 import com.mojang.logging.LogUtils
 import icu.takeneko.interact.Mod
-import io.ktor.network.selector.*
-import io.ktor.network.sockets.*
-import io.ktor.util.cio.*
-import io.ktor.utils.io.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.utils.io.CancellationException
+import io.ktor.websocket.*
+import kotlinx.coroutines.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.io.BufferedWriter
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentHashMap
@@ -21,14 +20,14 @@ import kotlin.io.path.deleteIfExists
 import kotlin.io.path.div
 import kotlin.io.path.writeText
 
+private val logger = LogUtils.getLogger()
+
 object NekoService : Thread("NekoInteractAPI-SocketServer") {
+    internal var currentConnection: WebSocketServerSession? = null
     private var communicationPort = Random().nextInt(30000, 65535)
-    private val logger = LogUtils.getLogger()
     private val services = mutableMapOf<String, Service>()
     private val responseHandlers = ConcurrentHashMap<String, (Response) -> Unit>()
-    private lateinit var outboundChannel: ByteWriteChannel
-    private lateinit var inboundChannel: ByteReadChannel
-    private lateinit var outboundWriter: BufferedWriter
+    private lateinit var httpServer: ApplicationEngine
     private val json = Json {
         ignoreUnknownKeys = true
         encodeDefaults = true
@@ -38,51 +37,40 @@ object NekoService : Thread("NekoInteractAPI-SocketServer") {
             return Mod.hasMCDR && this.isAlive
         }
 
+    suspend fun handleIncoming(line: String) {
+        val response = json.decodeFromString<Response>(line)
+        val id = response.requestId
+        if (responseHandlers.containsKey(id)) {
+            val fn = responseHandlers[id] ?: return
+            fn(response)
+            responseHandlers.remove(id)
+        } else {
+            val request = json.decodeFromString<Request>(line)
+            val resp = if (request.service in services) {
+                services[request.service]!!(request)
+            } else {
+                Response(request.service, request.requestId, mapOf("error" to "Service ${request.service} not found."))
+            }
+            sendResponse(resp)
+        }
+    }
+
     override fun run() {
         val file = Mod.privateDir / "port"
         file.deleteIfExists()
         file.createFile()
         file.writeText(communicationPort.toString())
-        runBlocking {
-            val selectorManager = SelectorManager(Dispatchers.IO)
-            val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", communicationPort)
-            logger.info("Serving NekoService at 127.0.0.1:$communicationPort")
-            while (true) {
-                val socket = serverSocket.accept()
-                inboundChannel = socket.openReadChannel()
-                outboundChannel = socket.openWriteChannel()
-                outboundWriter = outboundChannel.bufferedWriter()
-                launch {
-                    while (true){
-                        val line = inboundChannel.readUTF8Line() ?: continue
-                        val response = json.decodeFromString<Response>(line)
-                        val id = response.requestId
-                        if (responseHandlers.containsKey(id)){
-                            val fn = responseHandlers[id] ?: continue
-                            fn(response)
-                            responseHandlers.remove(id)
-                        }else{
-                            val request = json.decodeFromString<Request>(line)
-                            val resp = if (request.service in services){
-                                services[request.service]!!(request)
-                            } else {
-                                Response(request.service, request.requestId, mapOf("error" to "Service ${request.service} not found."))
-                            }
-                            sendResponse(resp)
-                        }
-                    }
-                }
-            }
-        }
+        httpServer = embeddedServer(CIO, port = communicationPort, host = "127.0.0.1", module = Application::module)
+        httpServer.start(wait = true)
     }
 
-    fun registerService(id:String, service: Service){
+    fun registerService(id: String, service: Service) {
         services[id] = service
     }
 
-    suspend fun sendResponse(response: Response){
+    private suspend fun sendResponse(response: Response) {
         withContext(Dispatchers.IO) {
-            outboundWriter.write(json.encodeToString<Response>(response) + "\n")
+            currentConnection?.send(json.encodeToString<Response>(response) + "\n")
         }
     }
 
@@ -90,11 +78,40 @@ object NekoService : Thread("NekoInteractAPI-SocketServer") {
         if (!available) {
             throw IllegalStateException("Service Unavailable!")
         }
-        outboundWriter.write(json.encodeToString<Request>(request) + "\n")
-        val future = CompletableFuture<Response>()
-        responseHandlers[request.requestId] = {
-            future.complete(it)
+        return runBlocking {
+            currentConnection?.send(json.encodeToString<Request>(request) + "\n")
+            val future = CompletableFuture<Response>()
+            responseHandlers[request.requestId] = {
+                future.complete(it)
+            }
+            future
         }
-        return future
     }
+}
+
+private fun Application.configureRouting() {
+    routing {
+        webSocket {
+            try{
+                NekoService.currentConnection?.cancel()
+                NekoService.currentConnection = this
+                logger.info("New Incoming Connection ${NekoService.currentConnection}, discarding old connection.")
+                for (frame in this.incoming) {
+                    frame as? Frame.Text ?: continue
+                    val text: String = frame.readText()
+                    if (text.startsWith("Heartbeat_PING_")) {
+                        this.send(text.replace("PING", "PONG"))
+                        continue
+                    }
+                    NekoService.handleIncoming(text)
+                }
+            }catch (_:CancellationException){
+                logger.info("Disconnecting $this.")
+            }
+        }
+    }
+}
+
+private fun Application.module() {
+    configureRouting()
 }
